bc91ccdd91eb74b8588928a1d5f1ca26813ed1a3
mnastasi9 <163416812+mnastasi9@users.noreply.github.com>
2024-09-04 12:54:18 -0300

    ahora estan bien puestos

--- src/app/page.js
+++ src/app/page.js
@@ -8,33 +8,6 @@ import Chat from '../components/Chat';
 import styles from './page.module.css';
 import Button from '@/components/button';
 
-export default function UsersRanking(){
-
-  //Le aclaro a la pagina o componente  que quiero usar el  hook useSocket
-  const {socket, isConnected} = useSocket();
-  useEffect(()=>{
-    //Para evitar que genere errores si no esta el socket
-    if(!socket)return;
-    socket.on('pingAll', (data) => {
-      console.log("Me llego el evento pingAll", data);
-    })
-
-
-  },[socket, isConnected]);
-
-  function handleClick() {
-    socket.emit('pingAll', {message: "feli 5cm"});
-  }
-
-  return(
-    <>
-      <h1>Soy la ruta /ranking/users</h1>
-      <Button onClick={handleClick} text="Enviar pingAll" />
-    </>
-  )
-}
-
-
 
 const ChatInterface = () => {
   const [messages, setMessages] = useState([]);
@@ -98,4 +71,4 @@ const ChatInterface = () => {
   );
 };
 
-
+export default ChatInterface;
--- src/app/ranking/users/page.js
+++ src/app/ranking/users/page.js
@@ -1,7 +1,31 @@
-export default function UsersRanking() {
-    return (
-      <div>
-          <h1>Hola desde ranking/users</h1>
-      </div>
-      )
+"use client"
+
+import { useSocket } from '@/hooks/useSocket';
+import { useEffect } from 'react';
+import Button from '@/components/button';
+
+export default function UsersRanking(){
+
+    //Le aclaro a la pagina o componente  que quiero usar el  hook useSocket
+    const {socket, isConnected} = useSocket();
+    useEffect(()=>{
+      //Para evitar que genere errores si no esta el socket
+      if(!socket)return;
+      socket.on('pingAll', (data) => {
+        console.log("Me llego el evento pingAll", data);
+      })
+  
+  
+    },[socket, isConnected]);
+  
+    function handleClick() {
+      socket.emit('pingAll', {message: "feli 5cm"});
+    }
+  
+    return(
+      <>
+        <h1>Soy la ruta /ranking/users</h1>
+        <Button onClick={handleClick} text="Enviar pingAll" />
+      </>
+    )
   }
