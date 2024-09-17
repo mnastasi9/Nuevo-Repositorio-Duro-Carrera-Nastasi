ee367067ac47ed6dd2b959cabc4bebae45797e75
mnastasi9 <163416812+mnastasi9@users.noreply.github.com>
2024-09-17 08:45:55 -0300

    conect message

--- src/app/ranking/page.js
+++ src/app/ranking/page.js
@@ -1,24 +1,43 @@
 "use client"
 import Button from "@/components/button";
+import { useEffect } from "react";
+import { useSocket } from "@/hooks/useSocket";
+import { useState } from "react";
 
 export default function ranking() {
+    const {socket, isConnected} = useSocket();
+    const [message, setMessage]= useState("");
 
-    function funcionBoton1() {
-        console.log("Hola");
+    useEffect(() => {
+        if(!socket) return;
+        socket.on("pingAll", (data) =>{
+            console.log("Mensaje recibido: ", data);
+        })
+
+        socket.on("newMessage", (data) =>{
+            console.log("Mensaje recibido de la sala: ", data);
+        })
+    }, [socket, isConnected]);
+
+    function handlePing() {
+        socket.emit("pingAll", {message})
     }
 
-    function funcionBoton2() {
-        console.log("Chau");
+    function handleSendMessage(){
+        socket.emit('sendMessage', {message: message})
+    }
+
+    function handleChangeInput(event){
+        setMessage(event.target.value)
     }
 
     return (
-      <div>
-          <h1>RANKING</h1>
-          <h1>RANKING</h1>
-          <h1>RANKING</h1>
-
-          <Button onClick={funcionBoton1} text="Botón 1"/>
-          <Button onClick={funcionBoton2} text="Botón 2"/>
-      </div>
+        <>
+            <h1>Soy la ruta/ranking/users</h1>
+            <Button onClick={handlePing} text="Ping all"/>
+            <Button onClick={() => socket.emit('joinRoom', {room: "chismesin"})} text="Conectar/Unirse a la sala"/>
+            <input onChange={handleChangeInput}/>
+            <Button onClick={handleSendMessage} text="Enviar mensaje"/>
+        </>
       )
   }
